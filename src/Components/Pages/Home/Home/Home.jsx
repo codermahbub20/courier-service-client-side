d244c16bc98717fb6b15809434a0f156abfd1455
codermahbub20 <mkrefat5@gmail.com>
2024-06-13 00:31:11 +0600

    Navbar created

--- src/Components/Pages/Home/Home/Home.jsx
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Navbar from "../../../../Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            Hello World
+            <Navbar></Navbar>
         </div>
     );
 };
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,11 +1,19 @@
 import {
     createBrowserRouter,
 } from "react-router-dom";
+import MainLayout from "../LayOuts/MainLayout";
 import Home from "../Components/Pages/Home/Home/Home";
+
 
 export const router = createBrowserRouter([
     {
         path: "/",
-        element: <Home />,
+        element: <MainLayout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />
+            }
+        ]
     },
 ]);
