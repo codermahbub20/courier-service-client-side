913f2820da939cfb95b161dfda72fea7e16683ca
codermahbub20 <mkrefat5@gmail.com>
2024-06-14 02:11:07 +0600

    Banner section add

--- src/Components/Pages/Home/Home/Home.jsx
+++ src/Components/Pages/Home/Home/Home.jsx
@@ -1,10 +1,13 @@
 import Navbar from "../../../../Shared/Navbar/Navbar";
+import Banner from "../Banner/Banner";
+
 
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner />
         </div>
     );
 };
