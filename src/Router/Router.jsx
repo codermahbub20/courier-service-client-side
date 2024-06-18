a1fc3394f437f400f216b883f761580f33195d28
codermahbub20 <mkrefat5@gmail.com>
2024-06-19 00:33:30 +0600

    Order Process section implemented

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,11 +3,14 @@ import {
 } from "react-router-dom";
 import MainLayout from "../LayOuts/MainLayout";
 import Home from "../Components/Pages/Home/Home/Home";
+import ErrorPage from "../ErrorPage/ErrorPage";
 
 
 export const router = createBrowserRouter([
+
     {
         path: "/",
+        errorElement: <ErrorPage />,
         element: <MainLayout />,
         children: [
             {
