65c24169df8686533c02ed14b526c6d830395d04
codermahbub20 <mkrefat5@gmail.com>
2024-06-21 01:13:45 +0600

    Navbar updated

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
 
+import { NavLink } from "react-router-dom";
 import MenuDropdown from "./MenuDropDown";
 
 
 const Navbar = () => {
 
+    const navlinks = <>
+        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/h">All Assignment</NavLink></li>
+        <li><NavLink to="/j">Create Assignment</NavLink></li>
+        <li><NavLink to="/n">All Submited Assignment</NavLink></li>
+        <li><NavLink to="/j">My Assignment</NavLink></li>
+    </>
 
     return (
 
@@ -14,32 +22,14 @@ const Navbar = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </div>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 text-lg rounded-box w-52">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <a>Parent</a>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </li>
-                        <li><a>Item 3</a></li>
+                        {navlinks}
                     </ul>
                 </div>
                 <img src="https://transp-nextjs.vercel.app/assets/imgs/template/logo.svg" alt="Logo" />
             </div>
             <div className="navbar-center hidden  lg:flex">
                 <ul className="menu menu-horizontal text-lg px-1">
-                    <li><a>Item 1</a></li>
-                    <li>
-                        <details>
-                            <summary>Parent</summary>
-                            <ul className="p-2">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
-                        </details>
-                    </li>
-                    <li><a>Item 3</a></li>
+                    {navlinks}
                 </ul>
             </div>
             <div className="navbar-end">
